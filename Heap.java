afb193ee40d4d8194f96c6a246910d5a1b40ee6a
Jonathan Diep <jonathandiep@hotmail.com>
2016-05-05 09:49:08 -0700

    insert and optimal insert complete

---
+++ Heap.java
@@ -0,0 +1,132 @@
+import java.util.Arrays;
+
+class Heap {
+  private final Integer CAPACITY = 2;
+
+  protected Integer size;
+  protected Integer[] heap;
+
+  public Heap() {
+    size = 0;
+    heap = new Integer[CAPACITY];
+  }
+
+  public Heap(Integer[] array) {
+    size = array.length;
+    heap = new Integer[array.length + 1];
+    System.arraycopy(array, 0, heap, 1, array.length);
+  }
+
+  public Integer size() {
+    return size;
+  }
+
+  public Integer[] resize() {
+    return Arrays.copyOf(heap, heap.length * 2);
+  }
+
+  public boolean hasParent(Integer i) {
+    return i > 1;
+  }
+
+  public Integer leftIndex(Integer i) {
+    return i * 2;
+  }
+
+  public Integer rightIndex(Integer i) {
+    return i * 2 + 1;
+  }
+
+  public boolean hasLeftChild(Integer i) {
+    return leftIndex(i) <= size && leftIndex(i) != null;
+  }
+
+  public boolean hasRightChild(Integer i) {
+    return rightIndex(i) <= size && rightIndex(i) != null;
+  }
+
+  public Integer parent(Integer i) {
+    return heap[parentIndex(i)];
+  }
+
+  public Integer parentIndex(Integer i) {
+    return i / 2;
+  }
+
+  public int swap(Integer index1, Integer index2) {
+    Integer temp = heap[index1];
+    heap[index1] = heap[index2];
+    heap[index2] = temp;
+    return 1;
+  }
+
+  public void insert(Integer data) {
+    if (size >= heap.length - 1) {
+      heap = this.resize();
+    }
+
+    size++;
+    heap[size] = data;
+    percolateUp(size);
+  }
+
+  public void percolateUp(int index) {
+    int compare = index;
+
+    while (hasParent(compare) && parent(compare) < heap[compare]) {
+      System.out.println("perc");
+      swap(compare, parentIndex(compare));
+      compare = parentIndex(compare);
+    }
+  }
+
+  public void heapify() {
+    for (int i = size() / 2; i >= 1; i--) {
+      percolateDown(i);
+    }
+  }
+
+  public void percolateDown(int index) {
+    int compare = index;
+
+    while (compare < size) {
+      if (hasRightChild(compare)) {
+        if (heap[leftIndex(compare)] > heap[rightIndex(compare)]) {
+          if (heap[compare] < heap[leftIndex(compare)]) {
+            swap(compare, leftIndex(compare));
+            compare = leftIndex(compare);
+          } else {
+            break;
+          }
+        } else {
+          if (heap[compare] < heap[rightIndex(compare)]) {
+            swap(compare, rightIndex(compare));
+            compare = rightIndex(compare);
+          } else {
+            break;
+          }
+        }
+      } else if (hasLeftChild(compare)) {
+        if (heap[compare] < heap[leftIndex(compare)]) {
+          swap(compare, leftIndex(compare));
+          compare = leftIndex(compare);
+        } else {
+          break;
+        }
+      } else {
+        break;
+      }
+    }
+
+  }
+
+  /*
+
+  need to count # of swaps
+
+  public boolean removal() {
+
+  }
+
+  */
+}
---
+++ Project2.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+class Project2 {
+  public static void main(String args[]) {
+    Integer[] arr = {11, 24, 3, 47, 57, 86, 37, 15, 90, 10};
+    Integer[] inc = new Integer[100];
+    for (Integer i = 1; i <= 100; i++) {
+      inc[i - 1] = i;
+    }
+    Heap heapInc = new Heap(inc);
+    heapInc.heapify();
+    System.out.println(Arrays.toString(heapInc.heap));
+    Heap test = new Heap(arr);
+    System.out.println(Arrays.toString(test.heap));
+    test.heapify();
+    System.out.println(Arrays.toString(test.heap));
+    /*
+    Heap empty = new Heap();
+    empty.insert(1);
+    System.out.println(empty.hasParent(1));
+    empty.insert(2);
+    empty.insert(3);
+    empty.insert(4);
+    empty.insert(5);
+    System.out.println(Arrays.toString(empty.heap));
+    */
+  }
+}
