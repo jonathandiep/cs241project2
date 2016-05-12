e6bb45416d160bc728c98fce59c0bbb7db91deb1
Jonathan Diep <jonathandiep@hotmail.com>
2016-05-12 14:35:27 -0700

    fixed + rand methods

--- Heap.java
+++ Heap.java
@@ -1,68 +1,68 @@
 import java.util.Arrays;
 
 class Heap {
-  private final Integer CAPACITY = 2;
+  private final int CAPACITY = 2;
 
-  protected Integer size;
-  protected Integer[] heap;
+  protected int size;
+  protected int[] heap;
 
   public Heap() {
     size = 0;
-    heap = new Integer[CAPACITY];
+    heap = new int[CAPACITY];
   }
 
-  public Heap(Integer[] array) {
+  public Heap(int[] array) {
     size = array.length;
-    heap = new Integer[array.length + 1];
+    heap = new int[array.length + 1];
     System.arraycopy(array, 0, heap, 1, array.length);
   }
 
-  public Integer size() {
+  public int size() {
     return size;
   }
 
-  public Integer[] resize() {
+  public int[] resize() {
     return Arrays.copyOf(heap, heap.length + 1);
   }
 
-  public boolean hasParent(Integer i) {
+  public boolean hasParent(int i) {
     return i > 1;
   }
 
-  public Integer leftIndex(Integer i) {
+  public int leftIndex(int i) {
     return i * 2;
   }
 
-  public Integer rightIndex(Integer i) {
+  public int rightIndex(int i) {
     return i * 2 + 1;
   }
 
-  public boolean hasLeftChild(Integer i) {
+  public boolean hasLeftChild(int i) {
     return leftIndex(i) <= size;
   }
 
-  public boolean hasRightChild(Integer i) {
+  public boolean hasRightChild(int i) {
     return rightIndex(i) <= size;
   }
 
-  public Integer parentIndex(Integer i) {
+  public int parentIndex(int i) {
     return i / 2;
   }
 
-  public Integer parent(Integer i) {
+  public int parent(int i) {
     return heap[parentIndex(i)];
   }
 
 
 
-  public int swap(Integer index1, Integer index2) {
-    Integer temp = heap[index1];
+  public int swap(int index1, int index2) {
+    int temp = heap[index1];
     heap[index1] = heap[index2];
     heap[index2] = temp;
     return 1;
   }
 
-  public int insert(Integer[] data) {
+  public int insert(int[] data) {
     int count = 0;
     for(int i = 0; i < data.length; i++) {
       if (size >= heap.length - 1) {
@@ -132,19 +132,17 @@ class Heap {
 
     return swapCount;
   }
-  
+
   public void removal() {
     if (size == 0) return;
-    if (heap[1] != null) {
+    if (size >= 1) {
       int pointer = heap.length - 1;
       heap[1] = heap[pointer];
-      Integer[] newHeap = new Integer[heap.length - 1];
+      int[] newHeap = new int[heap.length - 1];
       System.arraycopy(heap, 1, newHeap, 1, newHeap.length - 1);
       heap = newHeap;
       size--;
       heapify();
-    } else if (size == 1) {
-      heap[1] = null;
     }
   }
 
--- Project2.java
+++ Project2.java
@@ -1,31 +1,72 @@
 import java.util.Arrays;
+import java.util.Random;
 
 class Project2 {
   public static void main(String args[]) {
-    Integer[] inc = new Integer[100];
-    for (Integer i = 1; i <= 100; i++) {
+    fixedInts();
+    //randomInts();
+
+  }
+
+  static void randomInts() {
+    int[] insertionCount = new int[20];
+    int[] optimalCount = new int[20];
+
+    for (int i = 0; i < 20; i++) {
+      int[] rands = new int[100];
+      for (int j = 0; j < 100; j++) {
+        rands[j] = randInt();
+      }
+      Heap insert = new Heap();
+      insertionCount[i] = insert.insert(rands);
+      Heap optimal = new Heap(rands);
+      optimalCount[i] = optimal.heapify();
+    }
+
+    int insertTotal = 0;
+    int optimalTotal = 0;
+    for (int n : insertionCount) insertTotal += n;
+    for (int n : optimalCount) optimalTotal += n;
+    double insertAvg = insertTotal / insertionCount.length;
+    double optimalAvg = optimalTotal / optimalCount.length;
+
+    System.out.println("Average swaps for series of insertions: " + insertAvg);
+    System.out.println("Average swaps for optimal method: " + optimalAvg);
+
+  }
+
+  static void fixedInts() {
+    int[] inc = new int[100];
+    for (int i = 1; i <= 100; i++) {
       inc[i - 1] = i;
     }
-    Heap heapInc = new Heap(inc);
-    System.out.println("optimal");
-    System.out.println("#: " + heapInc.heapify());
-    System.out.println(Arrays.toString(heapInc.heap));
+
+    Heap insert = new Heap();
+    System.out.println("Series of Insertions");
+    System.out.println("Number of Swaps: " + insert.insert(inc));
+    System.out.println("Heap result: " + Arrays.toString(insert.heap));
     for (int i = 0; i < 10; i++) {
-      heapInc.removal();
+      insert.removal();
     }
-    System.out.println(Arrays.toString(heapInc.heap));
-    System.out.println();
+    System.out.println("Heap after 10 removals: " + Arrays.toString(insert.heap));
     System.out.println();
 
-    Integer[] arr = {11, 24, 3, 47, 57, 86, 37, 15, 90, 10};
-    Heap test = new Heap();
-    System.out.println("series of insertions");
-    System.out.println("#: " + test.insert(inc));
-    System.out.println(Arrays.toString(test.heap));
+    Heap optimal = new Heap(inc);
+    System.out.println("Optimal Method");
+    System.out.println("Number of Swaps: " + optimal.heapify());
+    System.out.println(Arrays.toString(optimal.heap));
     for (int i = 0; i < 10; i++) {
-      test.removal();
+      optimal.removal();
     }
-    System.out.println(Arrays.toString(test.heap));
+    System.out.println(Arrays.toString(optimal.heap));
+
+
+  }
+
+  static int randInt() {
+    Random rand = new Random();
+    int randNum = rand.nextInt((100 - 1) + 1) + 1;
+    return randNum;
   }
 }
 
@@ -33,9 +74,5 @@ class Project2 {
 
 todo:
 - create cli
-- create 20 sets of 100 randomly generated integers
-  - array of arrays?
-- get avg swaps for series of insertions
-- get avg swaps for optimal method
 
 */
